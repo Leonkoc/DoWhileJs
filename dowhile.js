b3ab8d7b073ee45fcfd55d72df0896d0a1c2f005
Leonkoc <leonkocc@hotmail.com>
2022-06-24 18:39:18 -0300

    dowhileJs

---
+++ dowhile.js
@@ -0,0 +1,8 @@
+let velocidade = 50;
+let aceleracao = 5;
+//FAÇA ENQUANTO VELOCIDADE FOR MENOR 100.
+do {
+  // BLOCO DE CÓDIGO //WHILE = ENQUANTO
+  console.log(`Acelerando: estamos a ${velocidade}Km/h`);
+  velocidade += aceleracao;
+} while (velocidade <= 100);
